21fda5e9409fdb66277598ec0a8eb689f3e8b8f0
pwq <panweiqi2016@125.com>
2024-11-23 11:03:08 +0800

    change T to tau

--- src/Option.py
+++ src/Option.py
@@ -10,7 +10,7 @@ class OptionType(Enum):
 
 class EuropeanOption:
     def __init__(self, 
-                 S0, K, T, r, q, sigma
+                 S0, K, tau, r, q, sigma
                  ):
         """
         Initializes a European option.
@@ -25,31 +25,31 @@ class EuropeanOption:
         """
         self.S0 = S0
         self.K = K
-        self.T = T
+        self.tau = tau
         self.r = r
         self.q = q
         self.sigma = sigma
 
     def _d1(self):
-        return (np.log(self.S0 / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.T
-                ) / (self.sigma * np.sqrt(self.T))
+        return (np.log(self.S0 / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.tau
+                ) / (self.sigma * np.sqrt(self.tau))
     
     def _d2(self):
-        return self._d1() - self.sigma * np.sqrt(self.T)
+        return self._d1() - self.sigma * np.sqrt(self.tau)
 
     def put_price(self):
         d1 = self._d1()
         d2 = self._d2()
         price = (
-            self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - 
-            self.S0 * np.exp(-self.q * self.T) * norm.cdf(-d1)
+            self.K * np.exp(-self.r * self.tau) * norm.cdf(-d2) -
+            self.S0 * np.exp(-self.q * self.tau) * norm.cdf(-d1)
         )
         return price
     def call_value(self):
         d1 = self._d1()
         d2 = self._d2()
         price = (
-            self.S0 * np.exp(-self.q * self.T) * norm.cdf(d1) -
-            self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
+            self.S0 * np.exp(-self.q * self.tau) * norm.cdf(d1) -
+            self.K * np.exp(-self.r * self.tau) * norm.cdf(d2)
         )
         return price
