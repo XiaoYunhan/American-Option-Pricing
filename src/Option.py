257b372094ba9880bf75afcb190c93253ed0e665
jong0077 <sengjong94@gmail.com>
2024-11-26 03:29:39 +0800

    Update dq_plus
    implemented my changes into the function

--- src/Option.py
+++ src/Option.py
@@ -1,55 +1,55 @@
-from scipy.stats import norm
 import numpy as np
+import scipy.stats as stats
 from enum import Enum
 
-from src.quadrature_nodes import QuadratureNodes
-
 class OptionType(Enum):
-    Call = 'call'
-    Put = 'put'
+    Call = 1
+    Put = 2
 
 class EuropeanOption:
-    def __init__(self, 
-                 S0, K, tau, r, q, sigma
-                 ):
-        """
-        Initializes a European option.
+    @staticmethod
+    def european_put_value(tau, s0, r, q, vol, strike):
+        """put option"""
+        if tau == 0:
+            return max(0, strike - s0)
+        d1 = EuropeanOption.d1(tau, s0, r, q, vol, strike)
+        d2 = EuropeanOption.d2(tau, s0, r, q, vol, strike)
+        return strike * np.exp(-r * tau) * stats.norm.cdf(-d2) - s0 * np.exp(-q * tau) * stats.norm.cdf(-d1)
 
-        Parameters:
-        S0 (float): Current stock price
-        K (float): Strike price
-        T (float): Time to maturity (in years)
-        r (float): Risk-free interest rate
-        q (float): Dividend yield
-        sigma (float): Volatility of the underlying asset
-        """
-        self.S0 = S0
-        self.K = K
-        self.tau = tau
-        self.r = r
-        self.q = q
-        self.sigma = sigma
+    @staticmethod
+    def european_call_value(tau, s0, r, q, vol, strike):
+        """put option"""
+        if tau == 0:
+            return max(0, s0 - strike)
+        d1 = EuropeanOption.d1(tau, s0, r, q, vol, strike)
+        d2 = EuropeanOption.d2(tau, s0, r, q, vol, strike)
+        return s0 * np.exp(-q * tau) * stats.norm.cdf(d1) - strike * np.exp(-r * tau) * stats.norm.cdf(d2)
 
-    def _d1(self):
-        return (np.log(self.S0 / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.tau
-                ) / (self.sigma * np.sqrt(self.tau))
-    
-    def _d2(self):
-        return self._d1() - self.sigma * np.sqrt(self.tau)
+    @staticmethod
+    def european_option_theta(tau, s0, r, q, vol, strike):
+        """put option theta"""
+        r = max(r, 1e-10)
+        tau = max(tau, 1e-10) # set tau negative
+        d1 = EuropeanOption.d1(tau, s0, r, q, vol, strike)
+        d2 = EuropeanOption.d2(tau, s0, r, q, vol, strike)
+        return r*strike * np.exp(-r * tau) * stats.norm.cdf(-d2) - q * s0 * np.exp(-q * tau)*stats.norm.cdf(-d1) \
+            - vol * s0 * np.exp(-q * tau) * stats.norm.pdf(d1)/(2 * np.sqrt(tau))
 
-    def put_price(self):
-        d1 = self._d1()
-        d2 = self._d2()
-        price = (
-            self.K * np.exp(-self.r * self.tau) * norm.cdf(-d2) -
-            self.S0 * np.exp(-self.q * self.tau) * norm.cdf(-d1)
-        )
-        return price
-    def call_value(self):
-        d1 = self._d1()
-        d2 = self._d2()
-        price = (
-            self.S0 * np.exp(-self.q * self.tau) * norm.cdf(d1) -
-            self.K * np.exp(-self.r * self.tau) * norm.cdf(d2)
-        )
-        return price
+    @staticmethod
+    def d1(tau, s0, r, q, vol, strike):
+        return np.log(s0 * np.exp((r-q)*tau)/strike)/(vol * np.sqrt(tau)) + 0.5*vol * np.sqrt(tau)
+
+    @staticmethod
+    def d2(tau, s0, r, q, vol, strike):
+        return EuropeanOption.d1(tau, s0, r, q, vol, strike) - vol * np.sqrt(tau)
+
+if __name__ == '__main__':
+    r = 0.04  # risk free
+    q = 0.04  # dividend yield
+    K = 100  # strike
+    S0 = 80  # underlying spot
+    sigma = 0.2  # volatility
+    T = 3.0  # maturity
+    put = EuropeanOption.european_option_value(T, S0, r, q, sigma, K)
+    call =  EuropeanOption.european_option_value(T, K, q, r, sigma , S0)
+    print("call = ", call, ", put = ", put)
