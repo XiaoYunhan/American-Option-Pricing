376bb659cb19d4989f1ee1e2e6d5284cf903e66f
pwq <panweiqi2016@125.com>
2024-11-28 03:09:40 +0800

    update compute_tanh_sinh_nodes_weights

--- src/quadrature_nodes.py
+++ src/quadrature_nodes.py
@@ -30,6 +30,21 @@ class QuadratureNodes:
         # Compute nodes and weights using numpy's leggauss function
         self.y_nodes, self.w_weights = np.polynomial.legendre.leggauss(self.l)
 
+    def compute_tanh_sinh_nodes_weights(self, m=10):
+        x_max = 1.0 - m * np.finfo(float).eps
+        t_max = np.arcsinh(2 * np.arctanh(x_max) / np.pi)
+
+        t_k = np.linspace(-t_max, t_max, self.l)
+
+        h = 2 * t_max / (self.l - 1)
+
+        arg = 0.5 * np.pi * np.sinh(t_k)
+        self.y_nodes = np.tanh(arg)  # x_k = tanh( (π / 2) * sinh(t_k) )
+
+        cosh_tk = np.cosh(t_k)
+        cosh_arg = np.cosh(arg)
+        self.w_weights = 0.5 * np.pi * cosh_tk / cosh_arg ** 2 * h  # w_k = (π / 2) * cosh(t_k) / cosh^2(arg) * h
+
     def get_nodes_and_weights(self):
         """
         Retrieve the computed quadrature nodes and weights.
